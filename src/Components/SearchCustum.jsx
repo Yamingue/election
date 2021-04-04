f3663abed71c1ee162b02444f52bb7edcbdf4252
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-04-04 14:11:17 +0200

    search =icon en search

--- src/Components/SearchCustum.jsx
+++ src/Components/SearchCustum.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles, Paper, InputBase,IconButton } from '@material-ui/core';
-import {Search as SearchIcon } from '@material-ui/icons';
+import {Search } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,7 +33,7 @@ const SearchCustum = ()=> {
         inputProps={{ 'aria-label': 'nom ou prenom' }}
       />
       <IconButton type="submit" className={classes.iconButton} aria-label="search">
-        <SearchIcon />
+        <Search />
       </IconButton>
     </Paper>
   );
