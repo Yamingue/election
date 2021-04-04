074a1c632fb0dbcc591a1da7ae351f427eff1b8f
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-04-04 11:19:43 +0200

    modification de partie pollitique

--- src/pages/Partis.jsx
+++ src/pages/Partis.jsx
@@ -15,52 +15,7 @@ export default class Partis extends React.Component {
                         <Icon className="fas fa-arrow-alt-circle-left" /> Retour
                     </IconButton>
                 </Link>
-                <Grid container spacing={6}>
-                    <Grid item xs={6}>
-                    <Card>
-                        <CardHeader
-                        title='MPS'
-                        />
-                        <CardMedia
-                        component='img'
-                        image='https://www.letchadanthropus-tribune.com/wp-content/uploads/2018/12/De%CC%81by-MPS.jpg'
-                        />
-                    </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card>
-                        <CardHeader
-                        title='MPS'
-                        />
-                        <CardMedia
-                        component='img'
-                        image='https://www.letchadanthropus-tribune.com/wp-content/uploads/2018/12/De%CC%81by-MPS.jpg'
-                        />
-                    </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card>
-                        <CardHeader
-                        title='MPS'
-                        />
-                        <CardMedia
-                        component='img'
-                        image='https://www.letchadanthropus-tribune.com/wp-content/uploads/2018/12/De%CC%81by-MPS.jpg'
-                        />
-                    </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card>
-                        <CardHeader
-                        title='MPS'
-                        />
-                        <CardMedia
-                        component='img'
-                        image='https://www.letchadanthropus-tribune.com/wp-content/uploads/2018/12/De%CC%81by-MPS.jpg'
-                        />
-                    </Card>
-                    </Grid>
-                </Grid>
+                
             </Container>
         );
     }
