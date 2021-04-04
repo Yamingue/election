371398f41ed39431ae79cfa060b7fd6d8c557151
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-04-04 13:17:25 +0200

    liste des parties

--- src/pages/Partis.jsx
+++ src/pages/Partis.jsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardMedia, Container, Grid, Icon, IconButton } from '@material-ui/core';
+import { Card, CardContent, CardHeader, CardMedia, Container, Grid, Icon, IconButton } from '@material-ui/core';
 import React from 'react'
 import { Link } from 'react-router-dom';
 
@@ -15,7 +15,108 @@ export default class Partis extends React.Component {
                         <Icon className="fas fa-arrow-alt-circle-left" /> Retour
                     </IconButton>
                 </Link>
-                
+                <Grid container spacing={6}>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/MPTR-2-300x212.jpg'
+                        />
+                        <CardContent>
+                        MPTR
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan-4-150x150.jpg'
+                        />
+                        <CardContent>
+                        UNDR
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan0002-2-150x150.jpg'
+                        />
+                        <CardContent>
+                        URD
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan-5-150x150.jpg'
+                        />
+                        <CardContent>
+                        ASTRE
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan-4-150x150.jpg'
+                        />
+                        <CardContent>
+                       FAR/PF
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan-1-150x150.jpg'
+                        />
+                        <CardContent>
+                        PRET
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan0001-1-150x150.jpg'
+                        />
+                        <CardContent>
+                        PNRDT LE REVEIL
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan0001-2-150x150.jpg'
+                        />
+                        <CardContent>
+                        PDI
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardMedia
+                            component='img'
+                            image='https://www.ceni.td/wp-content/uploads/2021/03/Scan-2-150x150.jpg'
+                        />
+                        <CardContent>
+                        UTPC
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+                </Grid>
             </Container>
         );
     }
